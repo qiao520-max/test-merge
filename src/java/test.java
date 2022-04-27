ab5e77741388c43b138c967ed8e2a100fdc30af9
漆加豪 <qijh@yinhai.com>
2022-04-27 16:21:29 +0800

    feat: 添加输出模块
    添加输出模块，具体包括输出一个1和输出另一个1

---
+++ src/java/test.java
@@ -0,0 +1,13 @@
+package src.java;
+
+/**
+ * @Program: test
+ * @Description:
+ * @Date: 2022/4/27
+ * @Author: solo.qiaoxiaobai.fun
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+}
